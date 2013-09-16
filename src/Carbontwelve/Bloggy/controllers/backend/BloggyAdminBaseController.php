6b7ffbebededbc9d90d472d2c1dfb9862b5585a7
carbontwelve <simon@photogabble.co.uk>
2013-09-16 22:42:17 +0100

    Fleshing out controller files

---
+++ src/Carbontwelve/Bloggy/controllers/backend/BloggyAdminBaseController.php
@@ -0,0 +1,16 @@
+<?php namespace Carbontwelve\Bloggy\Controllers\Backend;
+
+
+/**
+ * Class BloggyAdminBaseController
+ *
+ * @author  Simon Dann <simon@photogabble.co.uk>
+ * @since   0.0.2
+ * @package Carbontwelve\Bloggy\Controllers\Backend
+ */
+class BloggyAdminBaseController extends \Carbontwelve\Admin\Controllers\Backend\AdminBaseController
+{
+
+
+
+}
---
+++ src/Carbontwelve/Bloggy/controllers/backend/ContentAdminController.php
@@ -0,0 +1,15 @@
+<?php namespace Carbontwelve\Bloggy\Controllers\Backend;
+
+
+/**
+ * Class ContentAdminController
+ *
+ * @author  Simon Dann <simon@photogabble.co.uk>
+ * @since   0.0.2
+ * @package Carbontwelve\Bloggy\Controllers\Backend
+ */
+
+class ContentAdminController extends BloggyAdminBaseController
+{
+
+}
